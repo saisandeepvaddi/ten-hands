e15b9253e28996f153fc2dfc1bb1aba774570900
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2019-04-25 01:22:07 -0400

    add buttons

--- ui/src/components/Main.js
+++ ui/src/components/Main.js
@@ -1,14 +1,33 @@
 import React from "react";
 import styled from "styled-components";
-import { Colors } from "@blueprintjs/core";
+import { Colors, Button } from "@blueprintjs/core";
 
 const Container = styled.div`
   background: ${Colors.DARK_GRAY1};
   height: 100%;
+  display: flex;
+`;
+
+const Footer = styled.div`
+  width: 100%;
+  position: absolute;
+  bottom: 0;
+  display: flex;
+  padding: 1rem;
+  align-items: center;
+  & > button {
+    margin-left: auto;
+  }
 `;
 
 const Main = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Footer>
+        <Button icon="add" intent="success" text="New Command" large />
+      </Footer>
+    </Container>
+  );
 };
 
 export default Main;
--- ui/src/components/Sidebar.js
+++ ui/src/components/Sidebar.js
@@ -1,14 +1,28 @@
 import React from "react";
 import styled from "styled-components";
-import { Colors } from "@blueprintjs/core";
+import { Colors, Button } from "@blueprintjs/core";
 
 const Container = styled.div`
   background: ${Colors.DARK_GRAY2};
   height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding: 1rem;
 `;
 
 const Sidebar = () => {
-  return <Container />;
+  return (
+    <Container>
+      <Button
+        icon="add"
+        intent="success"
+        text="New Project"
+        large
+        style={{ width: "100%" }}
+      />
+    </Container>
+  );
 };
 
 export default Sidebar;
