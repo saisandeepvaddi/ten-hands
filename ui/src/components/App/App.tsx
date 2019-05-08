1f9c347cf9b33b8dc801f1e14a42ac765141b94d
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2019-05-08 00:58:16 -0400

    disable tslint in app.tsx for css import

--- ui/src/components/App/App.tsx
+++ ui/src/components/App/App.tsx
@@ -1,7 +1,12 @@
-import "@blueprintjs/core/lib/css/blueprint.css";
-import "@blueprintjs/icons/lib/css/blueprint-icons.css";
 import React from "react";
 import { ThemeProvider } from "../shared/Themes";
+
+// To disable no-submodule-imports, but tslint:disable:no-submodule-imports not working
+// See https://stackoverflow.com/questions/54071852/tslint-always-enforcing-no-submodule-imports-rule-even-if-disabled
+/* tslint:disable */
+import "@blueprintjs/core/lib/css/blueprint.css";
+import "@blueprintjs/icons/lib/css/blueprint-icons.css";
+/* tslint:enable */
 
 import { ProjectsProvider } from "../shared/Projects";
 import "./App.css";
