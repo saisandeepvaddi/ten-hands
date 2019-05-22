a4cae1195e37ef8fc0dbc8e730ecf31ca0a0acfd
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2019-05-22 01:40:10 -0400

    remove getting config file from user folder

--- app/server/index.ts
+++ app/server/index.ts
@@ -3,13 +3,11 @@ import app from "./app";
 
 import socketIO from "socket.io";
 import { JobManager } from "./services/job";
-import { getConfig } from "./setupConfig";
 
 export async function startServer() {
-  const config = await getConfig();
-  return new Promise((res, rej) => {
+  return new Promise(async (res, rej) => {
     try {
-      const port = config.port || process.env.PORT || 1010;
+      const port = process.env.PORT || 1010;
 
       const server = createServer(app);
 
