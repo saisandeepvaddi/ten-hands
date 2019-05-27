2e8feff31de77cd7ae2aa95863fa5a82032c5622
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2019-05-27 00:37:24 -0400

    add gulp for building releases

---
+++ gulpfile.js
@@ -0,0 +1,87 @@
+const { series, src, dest, parallel } = require("gulp");
+const { task, exec, stream } = require("gulp-execa");
+const del = require("del");
+const path = require("path");
+
+/* BUILDING TASKS */
+
+const buildUIForBrowser = task("yarn build:browser", {
+  cwd: path.join(__dirname, "ui")
+});
+
+const buildUIForElectron = task("yarn build:electron", {
+  cwd: path.join(__dirname, "ui")
+});
+
+const buildServerForElectron = task("yarn build", {
+  cwd: path.join(__dirname, "app")
+});
+
+const buildDesktopAppInstallers = task("yarn build:installers", {
+  cwd: path.join(__dirname, "app")
+});
+
+const buildCLI = task("yarn build", {
+  cwd: path.join(__dirname, "cli")
+});
+
+/* CLEANING TASKS */
+
+const cleanAppBuild = async () => {
+  return await del(["./app/build/**/*", "!./app/build"]);
+};
+
+const cleanAppDist = async () => {
+  return await del(["./app/dist/**/*", "!./app/dist"]);
+};
+
+const cleanDesktopFinalDist = async () => {
+  return await del(["./dist/desktop/**/*", "!./dist/desktop"]);
+};
+
+const cleanCLIFinalDist = async () => {
+  return await del(["./dist/cli/**/*", "!./dist/cli"]);
+};
+
+const cleanCLIBuild = async () => {
+  return await del(["./cli/build/**/*", "!./cli/build"]);
+};
+
+/* MOVE BUILD FILES TO THEIR FINAL DESTINATIONS. */
+
+const moveUIBuilds = async () => {
+  // Electron-Builder runs in ./app folder, so move UI build there before building installer
+  return src("./ui/build/**/*").pipe(dest("./app/build/ui"));
+};
+
+const moveDesktopBuildToFinalDist = async () => {
+  return src("./app/dist/**/*").pipe(dest("./dist/desktop"));
+};
+
+const moveCLIBuildToFinalDist = async () => {
+  return src("./cli/build/**/*").pipe(dest("./dist/cli"));
+};
+
+const delay = time => {
+  return async () => {
+    return new Promise(resolve => {
+      setTimeout(resolve, time);
+    });
+  };
+};
+
+exports.buildDesktop = series(
+  parallel(cleanAppBuild, cleanAppDist, cleanDesktopFinalDist),
+  parallel(buildUIForElectron, buildServerForElectron),
+  moveUIBuilds,
+  buildDesktopAppInstallers,
+  moveDesktopBuildToFinalDist,
+  delay(2000),
+  cleanAppDist
+);
+
+exports.buildCLI = series(
+  parallel(cleanCLIBuild, cleanCLIFinalDist),
+  buildCLI,
+  moveCLIBuildToFinalDist
+);
