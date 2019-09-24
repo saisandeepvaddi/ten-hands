9d9221e4d3070516339a7cc2d13c662b06773df3
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2019-09-23 23:27:29 -0400

    add delete project cypress task

--- ui/cypress/integration/projects.spec.js
+++ ui/cypress/integration/projects.spec.js
@@ -1,13 +1,19 @@
 import { getFakeProjects, getFakePackageJson } from "../support/generate";
 
 describe("Test Project Add/Remove", () => {
+  const fakeProjects = getFakeProjects(5);
   before(() => {
     cy.visit("/");
     cy.server();
     cy.route({
       method: "GET",
       url: "/projects",
-      response: getFakeProjects(5)
+      response: fakeProjects
+    });
+    cy.route({
+      method: "DELETE",
+      url: "/projects",
+      response: {}
     });
   });
   it("Tests new project upload", () => {
@@ -64,5 +70,56 @@ describe("Test Project Add/Remove", () => {
       expect(oneWithAddedProjectName).to.have.length(1);
     });
     cy.getByTestId("active-project-name").should("have.text", fakeProject.name);
+  });
+
+  it.only("Removes project", () => {
+    const secondProject = fakeProjects[1];
+    cy.getAllByTestId("project-name").then(sidebarProjectnames => {
+      const projectSidebarItem = cy.wrap(
+        Array.from(sidebarProjectnames).filter(
+          x => x.textContent === secondProject.name
+        )
+      );
+      projectSidebarItem.click();
+    });
+
+    cy.getByTestId("project-settings-button").click();
+    cy.getByTestId("delete-project-menu-item").click();
+    cy.getByTestId("delete-project-warning").should(
+      "have.text",
+      `Are you sure you want to delete project ${secondProject.name}?`
+    );
+    cy.getByText(/yes, delete/i).click();
+    cy.wait(2000);
+
+    cy.getAllByTestId("project-name").then(sidebarProjectnames => {
+      const oneWithRemovedProjectName = Array.from(sidebarProjectnames).filter(
+        x => x.textContent === secondProject.name
+      );
+      expect(oneWithRemovedProjectName).to.have.length(0);
+    });
+    cy.getByTestId("active-project-name").should(
+      "not.have.text",
+      secondProject.name
+    );
+  });
+
+  it.only("Deletes all projects", () => {
+    let projectsCount = 2;
+    const fakeProjects = getFakeProjects(projectsCount);
+    cy.route({
+      method: "GET",
+      url: "/projects",
+      response: fakeProjects
+    });
+
+    while (projectsCount-- > 0) {
+      cy.getByTestId("project-settings-button").click();
+      cy.getByTestId("delete-project-menu-item").click();
+      cy.getByText(/yes, delete/i).click();
+      cy.wait(2000);
+    }
+
+    cy.queryByTestId("no-projects-message").should("exist");
   });
 });
