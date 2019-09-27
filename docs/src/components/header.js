f3db775d2624bda2a594c21cff8046ea32b03625
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2019-09-26 23:32:44 -0400

    update website

--- docs/src/components/header.js
+++ docs/src/components/header.js
@@ -1,22 +1,23 @@
 import { Link } from "gatsby";
 import PropTypes from "prop-types";
 import React from "react";
+import logo from "../images/logo.png";
 
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#0A6640`,
-      marginBottom: `1.45rem`,
+      background: `#394B59`,
     }}
   >
     <div
       style={{
         margin: `0 auto`,
         maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
+        padding: `1rem`,
       }}
+      className="d-flex align-center"
     >
-      <h1 style={{ margin: 0 }}>
+      <div style={{ margin: 0 }}>
         <Link
           to="/"
           style={{
@@ -24,9 +25,17 @@ const Header = ({ siteTitle }) => (
             textDecoration: `none`,
           }}
         >
-          {siteTitle}
+          <div className="d-flex align-center">
+            <img src={logo} width="50" height="auto" alt="Ten Hands Logo" />
+            <span
+              className="px-1"
+              style={{ fontSize: "1.2rem", fontWeight: 600 }}
+            >
+              {siteTitle}
+            </span>
+          </div>
         </Link>
-      </h1>
+      </div>
     </div>
   </header>
 );
--- docs/src/pages/index.js
+++ docs/src/pages/index.js
@@ -3,11 +3,12 @@ import React from "react";
 import Layout from "../components/layout";
 import Image from "../components/image";
 import SEO from "../components/seo";
+import demo_img from "../images/demo_dark.jpg";
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Ten Hands" />
-    <p className="text-center">One place to run your command-line tasks</p>
+    <p className="text-center py-2">One place to run your command-line tasks</p>
     <div className="links text-center">
       <a
         className="download"
@@ -21,7 +22,15 @@ const IndexPage = () => (
       </a>
     </div>
     <div className="py-2"></div>
-    <Image />
+    <div className="d-flex justify-center">
+      <img
+        src={demo_img}
+        width="100%"
+        height="auto"
+        alt="Ten Hands demo image"
+      />
+    </div>
+    {/* <Image /> */}
     <div className="py-2"></div>
   </Layout>
 );
