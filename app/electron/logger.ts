616cab5d987aa6b197102160f85718abe56b4410
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2019-11-04 20:05:45 -0500

    remove console for logger transport

--- app/electron/logger.ts
+++ app/electron/logger.ts
@@ -2,6 +2,6 @@ const Logger = require("just-enough-logger").default;
 import { CONFIG_FILES } from "../shared/config";
 
 export const log = new Logger({
-  transports: ["file", "console"],
+  transports: ["file"],
   file: CONFIG_FILES.logFile
 });
