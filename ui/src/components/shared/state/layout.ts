0892159f565f8d42ff18272aae8ef1f82d55b215
Sai Sandeep Vaddi <saisandeepvaddi@gmail.com>
2020-10-12 23:43:29 -0400

    fix: sider not showing if 'sider-width' not in localStorage

--- ui/src/components/shared/state/layout.ts
+++ ui/src/components/shared/state/layout.ts
@@ -1,7 +1,18 @@
 import { atom } from "recoil";
-import { getItem } from "../../../utils/storage";
+import { getItem, setItem } from "../../../utils/storage";
+
+let defaultSiderWidth = Number(getItem("sider-width"));
+
+if (
+  !defaultSiderWidth ||
+  defaultSiderWidth === 0 ||
+  Number.isNaN(defaultSiderWidth)
+) {
+  setItem("sider-width", 300);
+  defaultSiderWidth = 300;
+}
 
 export const siderWidthAtom = atom<number>({
   key: "siderWidth",
-  default: Number(getItem("sider-width")) ?? 300,
+  default: defaultSiderWidth ?? 300,
 });
